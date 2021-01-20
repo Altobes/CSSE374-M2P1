0cde423812ffb1f22feb3c252c3b66518b037f07
Altobes <altobes@rose-hulman.edu>
2021-01-20 18:09:59 -0500

    Command and Response objects

---
+++ src/Command.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Command {
+    int controller_id;
+    int coffee_machine_id;
+    int orderID;
+    String DrinkName;
+    String Requesttype;
+    ArrayList<ArrayList<String>> Options;
+}
---
+++ src/Response.java
@@ -0,0 +1,6 @@
+public class Response {
+    int orderID;
+    int status;
+    String errordesc;
+    int errorcode;
+}
